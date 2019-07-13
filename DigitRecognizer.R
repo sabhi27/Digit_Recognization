706e48dc6849047bb303fd06c63291e5d6928074
Abhishek Singh <singh.ab2709@gmail.com>
2019-07-14 00:57:21 +0530

    Update DigitRecognizer.R

--- DigitRecognizer.R
+++ DigitRecognizer.R
@@ -1,67 +1,68 @@
-library(readr)
-library(nnet)
-library(gridExtra)
-
-model_Name <- "DigitRecognizer"
-train <- read.csv("mnist_train.csv")
-test <- read.csv("mnist_test.csv")
+# Libraries
+  library(readr)
+  library(nnet)
+  library(gridExtra)
 
 
-cat(sprintf("Training set has %d rows and %d columns\n", nrow(train), ncol(train)))
-cat(sprintf("Test set has %d rows and %d columns\n", nrow(test), ncol(test)))
+  model_Name <- "DigitRecognizer"
 
-X <- train[,-1]
-Y <- train[,1]
-train_label <- train[,1]
+# Importing data
+  train <- read.csv("mnist_train_42k.csv")
+  test <- read.csv("mnist_test_28k.csv")
+
+# Predictos & target variables
+  predictors <- train[,-1]
+  target <- train[,1]
+  train_label <- as.factor(train[,1])
+
+# PCA
+  predictors_reduced <- predictors/255
+  predictors_cov <- cov(predictors_reduced)
+  pca_predictors <- prcomp(predictors_cov)
 
 
-X_reduced <- X/255
-X_cov <- cov(X_reduced)
-pca_X <- prcomp(X_cov)
+  variance_explained <- as.data.frame(pca_predictors$sdev^2/sum(pca_predictors$sdev^2))
+  variance_explained <- cbind(c(1:784),variance_explained,cumsum(variance_explained[,1]))
+  colnames(variance_explained) <- c("No_of_Principal_Components","Individual_Variance_Explained",
+                            "Cumulative_Variance_Explained")
 
+# Plot
+  plot(variance_explained$No_of_Principal_Components,variance_explained$Cumulative_Variance_Explained, 
+       xlim = c(0,100),type='b',pch=16,xlab = "Principal Components",
+       ylab = "Cumulative Variance Explained",main = 'Principal Components vs Cumulative Variance')
 
-variance_explained <- as.data.frame(pca_X$sdev^2/sum(pca_X$sdev^2))
-variance_explained <- cbind(c(1:784),variance_explained,cumsum(variance_explained[,1]))
-colnames(variance_explained) <- c("No_of_Principal_Components","Individual_Variance_Explained",
-                          "Cumulative_Variance_Explained")
+  variance_explained_summary <- variance_explained[seq(0,100,5),]
 
-plot(variance_explained$No_of_Principal_Components,variance_explained$Cumulative_Variance_Explained, 
-     xlim = c(0,100),type='b',pch=16,xlab = "Principal Components",
-     ylab = "Cumulative Variance Explained",main = 'Principal Components vs Cumulative Variance')
+# png("datatable_variance_explained.png",height = 800,width =1000)
+# p <-tableGrob(variance_explained_summary)
+# 
+# grid.arrange(p)
+# dev.off()
+  predictors_final <- as.matrix(predictors_reduced) %*% pca_predictors$rotation[,1:45]
+  
+  train_label <- as.factor(train_label)
+  target <- class.ind(target)
+# print(predictors[1:5,1:5])
+# print(target[1:5,])
 
-variance_explained_summary <- variance_explained[seq(0,100,5),]
-variance_explained_summary
-png("datatable_variance_explained.png",height = 800,width =1000)
-p <-tableGrob(variance_explained_summary)
+# Model
+  model_final <- nnet(predictors_final,target,size=150,softmax=TRUE,maxit=100,MaxNWts = 80000)
 
-grid.arrange(p)
-dev.off()
-X_final <- as.matrix(X_reduced) %*% pca_X$rotation[,1:45]
+# test_target <- as.factor(test[,1])
 
-train_label <- as.factor(train_label)
-Y <- class.ind(Y)
-print(X[1:5,1:5])
-print(Y[1:5,])
+  test_reduced <- test/255
+  test_final <- as.matrix(test_reduced) %*%  pca_predictors$rotation[,1:45]
+  
+  prediction <- predict(model_final,test_final,type="class")
+  prediction <- as.data.frame(prediction)
+  final_prediction<- cbind(as.data.frame(1:nrow(prediction)),prediction)
+  save.image(file=paste(model_Name,"-Model.RData",sep=''))
 
-final_seed <- 150
-set.seed(final_seed)
-model_final <- nnet(X_final,Y,size=150,softmax=TRUE,maxit=100,MaxNWts = 80000)
+# actual <- as.double(unlist(test[,1]))
 
-testlabel <- as.factor(test[,1])
+# accuracy = round(mean(actual == prediction) * 100, 2)
+# accuracy
 
-test_reduced <- test/255
-test_final <- as.matrix(test_reduced[,-1]) %*%  pca_X$rotation[,1:45]
-
-prediction <- predict(model_final,test_final,type="class")
-prediction <- as.data.frame(prediction)
-final_prediction<- cbind(as.data.frame(1:nrow(prediction)),prediction)
-save.image(file=paste(model_Name,"-Model.RData",sep=''))
-
-actual <- as.double(unlist(test[,1]))
-
-accuracy = round(mean(actual == prediction) * 100, 2)
-accuracy
-
-as <- cbind(actual,prediction)
-colnames(final_prediction) <- c("ImageId","Label")
-write.csv(final_prediction,file="prediction.csv",row.names=FALSE)
+# as <- cbind(actual,prediction)
+  colnames(final_prediction) <- c("ImageId","Label")
+  write.csv(final_prediction,file="prediction.csv",row.names=FALSE)
