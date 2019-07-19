2f7f4144225292de5dd0203d132ed7ba4b61b6ec
Abhishek Singh <singh.ab2709@gmail.com>
2019-07-19 17:28:22 +0530

    Update DigitRecognizer.R

--- DigitRecognizer.R
+++ DigitRecognizer.R
@@ -18,40 +18,65 @@
 # PCA
   predictors_reduced <- predictors/255
   predictors_cov <- cov(predictors_reduced)
-  pca_predictors <- prcomp(predictors_cov)
+  prin_comp <- prcomp(predictors_cov)
 
+  names(prin_comp)
+  
+# Output the mean of variables
+  View(prin_comp$center)
+  
+# Output the SD of variables
+  prin_comp$scale
+  
+# Rotation matrix where each column contains PC loading vector
+  View(prin_comp$rotation)
 
-  variance_explained <- as.data.frame(pca_predictors$sdev^2/sum(pca_predictors$sdev^2))
-  variance_explained <- cbind(c(1:784),variance_explained,cumsum(variance_explained[,1]))
-  colnames(variance_explained) <- c("No_of_Principal_Components","Individual_Variance_Explained",
+# Plot esultant principal components 
+  biplot(prin_comp, scale = 0)
+  
+# Compute SD of each Principal component
+  SD <- prin_comp$sdev
+  
+# Compute Variance
+  Var <- SD^2
+  
+# Proportion of Variance Explained
+  variance_explained <- Var/sum(Var)
+  variance_explained_df <- data.frame(c(1:784),variance_explained,cumsum(variance_explained))
+  colnames(variance_explained_df) <- c("No_of_Principal_Components","Individual_Variance_Explained",
                             "Cumulative_Variance_Explained")
 
-# Plot
-  plot(variance_explained$No_of_Principal_Components,variance_explained$Cumulative_Variance_Explained, 
-       xlim = c(0,100),type='b',pch=16,xlab = "Principal Components",
-       ylab = "Cumulative Variance Explained",main = 'Principal Components vs Cumulative Variance')
+# Scree Plot
+  plot(variance_explained,xlab = "Principal Components",
+       ylab = "Proportion of Variance Explained",xlim=c(1,100),
+       type="b")
+  
+# Cummulative scree plot
+  plot(cumsum(variance_explained), xlab = "Principal Component",
+  ylab = "Cumulative Proportion of Variance Explained",
+  type = "b")
 
-  variance_explained_summary <- variance_explained[seq(0,100,5),]
 
 # png("datatable_variance_explained.png",height = 800,width =1000)
 # p <-tableGrob(variance_explained_summary)
 # 
 # grid.arrange(p)
 # dev.off()
-  predictors_final <- as.matrix(predictors_reduced) %*% pca_predictors$rotation[,1:45]
+  
+  predictors_final <- as.matrix(predictors_reduced) %*% prin_comp$rotation[,1:260]
   
   train_label <- as.factor(train_label)
   target <- class.ind(target)
 # print(predictors[1:5,1:5])
 # print(target[1:5,])
 
-# Model
+# Neural Network Model
   model_final <- nnet(predictors_final,target,size=150,softmax=TRUE,maxit=100,MaxNWts = 80000)
 
 # test_target <- as.factor(test[,1])
 
   test_reduced <- test/255
-  test_final <- as.matrix(test_reduced) %*%  pca_predictors$rotation[,1:45]
+  test_final <- as.matrix(test_reduced) %*%  prin_comp$rotation[,1:260]
   
   prediction <- predict(model_final,test_final,type="class")
   prediction <- as.data.frame(prediction)
@@ -66,3 +91,7 @@
 # as <- cbind(actual,prediction)
   colnames(final_prediction) <- c("ImageId","Label")
   write.csv(final_prediction,file="prediction.csv",row.names=FALSE)
+
+# options(repos = BiocInstaller::biocinstallRepos())
+# getOption("repos")
+# options(rsconnect.http = "rcurl")
